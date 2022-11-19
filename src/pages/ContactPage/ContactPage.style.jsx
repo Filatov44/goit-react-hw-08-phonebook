a7436d2f5a64d3e28b7cf7abe6eb9efb599d13c3
Filatov44 <filatov300@gmail.com>
2022-11-19 20:08:43 +0200

    fix

--- src/pages/ContactPage/ContactPage.style.jsx
+++ src/pages/ContactPage/ContactPage.style.jsx
@@ -2,11 +2,7 @@
 
 import styled from 'styled-components';
 export const StyledContactsContainer = styled.div`
-  /* background-image: url("../../img/phonebookHomePageBG.jpg"); */
-  /* background-image: url('https://img.freepik.com/premium-vector/vector-blurred-background-with-smooth-gradient-flag-ukraine-template-design-with-place-text_465179-219.jpg'); */
-  /* background-size: cover; */
   background-size: contain;
-
   height: 100%;
   width: 100%;
 `;
